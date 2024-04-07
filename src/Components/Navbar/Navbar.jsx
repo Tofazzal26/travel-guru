85db344dbf7f4e215bfe697a7045e04d50222079
Tofazzal26 <afranislamabir6789@gmail.com>
2024-04-07 13:01:27 +0600

    navbar style finish

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,74 +1,87 @@
-import React from "react";
 import { NavLink } from "react-router-dom";
-
+import logo from "../../assets/icons/logo.png";
 const Navbar = () => {
   const nabLinks = (
     <>
       <NavLink to="/">
-        <li>News</li>
+        <li className="text-lg font-semibold">News</li>
       </NavLink>
       <NavLink to="/">
-        <li>Destination</li>
+        <li className="text-lg font-semibold">Destination</li>
       </NavLink>
       <NavLink to="/">
-        <li>Blog</li>
+        <li className="text-lg font-semibold">Blog</li>
       </NavLink>
       <NavLink to="/">
-        <li>Contact</li>
+        <li className="text-lg font-semibold">Contact</li>
       </NavLink>
     </>
   );
 
   return (
-    <div className="container mx-auto">
-      <div className="navbar bg-base-100">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+    <div className="pt-6">
+      <div className="container mx-auto">
+        <div className="navbar ">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                {nabLinks}
+              </ul>
+            </div>
+            <img className="btn btn-ghost text-xl mr-10" src={logo} alt="" />
+            <label className="input bg-[#FFFFFF33] border-2 border-white text-white input-bordered flex items-center gap-2">
+              <input
+                type="text"
+                className="text-[#FFFFFF] lg:w-[350px]"
+                placeholder="Search your Destination..."
+              />
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="w-6 h-6 "
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
+                  fillRule="evenodd"
+                  d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                  clipRule="evenodd"
                 />
               </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-            >
+            </label>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal ml-[330px] text-white space-x-12 px-1">
               {nabLinks}
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">Travel Guru</a>
-          <label className="input input-bordered flex items-center gap-2">
-            <input type="text" className="grow" placeholder="Search" />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              className="w-4 h-4 opacity-70"
-            >
-              <path
-                fillRule="evenodd"
-                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </label>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal space-x-8 px-1">{nabLinks}</ul>
-        </div>
-        <div className="navbar-end">
-          <a className="btn">Login</a>
+          <div className="navbar-end">
+            <button className="btn text-black text-lg border-none bg-[#F9A51A]">
+              Login
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,9 +1,14 @@
-import React from "react";
 import Navbar from "../Navbar/Navbar";
-
+import bannerImg from "../../assets/images/Rectangle 1.png";
 const Root = () => {
   return (
-    <div>
+    <div
+      style={{
+        backgroundImage: `linear-gradient(rgba(0,0,0,0.60), rgba(0,0,0,0.60)),url(${bannerImg})`,
+        height: "100vh",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
       <Navbar />
     </div>
   );
